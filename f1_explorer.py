a0a846547f915868a47166e3ebee00b9d623f6f6
Josh Maddigan <josh.maddigan@gmail.com>
2026-05-23 17:21:08 -0400

    add header line

--- f1_explorer.py
+++ f1_explorer.py
@@ -7,6 +7,6 @@ data = response.json()
 
 # print(data["MRData"]["StandingsTable"]["StandingsLists"][0]["DriverStandings"][0]["points"])
 
-
+print(f"{'Pos':<5} {'Driver':^31} {'Team':<15} {'Points'} {'Wins'}")
 for driver in data["MRData"]["StandingsTable"]["StandingsLists"][0]["DriverStandings"]:
-    print(f"{driver['position']}. {driver["Driver"]["givenName"]:<15} {driver['Driver']['familyName']:<15} {driver['Constructors'][0]['name']:<18} {driver['points']:>5} {driver['wins']:>5}")
+    print(f"{driver['position']:<5} {driver["Driver"]["givenName"]:<15} {driver['Driver']['familyName']:<15} {driver['Constructors'][0]['name']:<18} {driver['points']:<4} {driver['wins']:<2}")
